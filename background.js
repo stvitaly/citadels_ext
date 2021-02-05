9a42059eca55d6df8eeb7bf500d2ae2e2c6d21a7
Vitaly Greditor <stvitaly@gmail.com>
2021-02-05 11:29:34 +0200

    first commit

--- background.js
+++ background.js
@@ -1,5 +1,6 @@
 let color = '#3aa757';
 
+//TODO : @
 chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.sync.set({ color });
   console.log('Default background color set to %cgreen', `color: ${color}`);
